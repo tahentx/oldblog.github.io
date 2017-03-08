b6db49ec6793ec849a84b09d4a56a000a978cccd
Todd Hendricks <hendricks.ta@gmail.com>
2017-03-08 13:56:40 -0600

    migrating folders from codeup dev environment to personal site dev environment.

---
+++ js/inline.js
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html>
+<head>
+</head>
+<body>
+	<script>
+	// message = "Hello from inline JavaScript"
+	console.log(message)
+	</script>
+</body>
+</html>
---
+++ js/internal_js.js
@@ -0,0 +1,9 @@
+<!DOCTYPE>
+	<html>
+	<head>
+	    <title>Internal JS</title>
+	</head>
+	<body>
+	    <script src="js/external.js"></script>
+	</body>
+</html>
